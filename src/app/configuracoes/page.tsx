aa6644b7f6d10e7c8ec90123cf6ea0be868c0d06
Fabrício Peres <128615061+DevPeress@users.noreply.github.com>
2025-04-28 19:49:11 -0300

    Add files via upload

---
+++ src/app/configuracoes/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import Pagina  from "@/components/pagina";
+import AuthGuard from "@/components/authguard";
+import { SideBar } from "@/components/sidebar";
+import { useEffect } from "react";
+
+export default function Products() {
+    useEffect(() => {
+      document.title = "Configurações"
+    },[])
+
+    return (
+        <>
+            <AuthGuard />
+            <SideBar />
+            <Pagina> 
+                <h1 className="absolute top-5 left-[2.7085vw] text-[2vw] text-[#111827] select-none">Configurações</h1>
+                <div></div>
+            </Pagina>
+        </>
+    )
+}
