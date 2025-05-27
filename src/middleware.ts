04b6f28b616f3997197994a68d84047c922e9fa4
Peres <fabricioperesdsantos@gmail.com>
2025-05-27 20:48:02 -0300

    Ajuste do middle

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,5 +16,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/', '/inicio', '/conta', '/configuracoes', '/compradores', '/registrar', '/cadastro'],
+  matcher: ['/', '/inicio', '/conta', '/configuracoes', '/compradores', '/registrar'],
 };
