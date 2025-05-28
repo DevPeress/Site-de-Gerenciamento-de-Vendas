0f6f0b34eca64f1f7bd43af8f6ea3aef8f356615
Peres <fabricioperesdsantos@gmail.com>
2025-05-28 13:51:20 -0300

    Responsividade do celular

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,10 +10,10 @@ export const metadata: Metadata = {
 export default function NotFound() {
     return(
         <div className="flex absolute w-full h-full dark:bg-black top-0 bottom-0 left-0 right-0 m-auto items-center justify-center select-none text-black dark:text-[#FFFFFF]">
-            <h1 className="absolute text-[1.5vw] top-[5vw]">404: Página não encontrada!</h1>
-            <h2 className="absolute text-[.8vw] top-[8vw]">Ou você tentou alguma rota suspeita ou chegou aqui por engano. Seja qual for, tente usar a navegação</h2>
+            <h1 className="absolute text-[5vw] md:text-[1.5vw] top-[10vw] md:top-[5vw]">Página não encontrada!</h1>
+            <h2 className="absolute text-[1.8vw] md:text-[.8vw] top-[18vw] md:top-[8vw]">Ou você tentou alguma rota suspeita ou chegou aqui por engano. Seja qual for, tente usar a navegação</h2>
             <Image
-                className="absolute w-[50vw]"
+                className="absolute w-[80vw] md:w-[50vw]"
                 src="/404.svg"
                 alt="Logo 404 erro"
                 width={180}
@@ -22,16 +22,16 @@ export default function NotFound() {
             />
 
             <Link href="/inicio" className="flex absolute items-center justify-center bottom-[7vw]">
-                <div className="flex absolute w-[10vw] h-[2.5vw] bg-[#5048E5] dark:bg-[#333333] items-center justify-center rounded-[.5vw] hover:scale-110">
+                <div className="flex absolute w-[30vw] md:w-[10vw] h-[8vw] md:h-[2.5vw] bg-[#5048E5] dark:bg-[#333333] items-center justify-center rounded-[.5vw] hover:scale-110">
+                    <h2 className="text-[#FFFFFF] text-[3vw] md:text-[.8vw]">Página Inicial</h2>
                     <Image
-                        className="absolute w-[1.5vw] left-[1vw]"
+                        className="absolute w-[3vw] md:w-[1.5vw] left-[1vw]"
                         src="/arrow-back.svg"
                         alt="Seta para voltar ao painel"
                         width={180}
                         height={38}
                         priority
                     />
-                    <h2 className="absolute text-[#FFFFFF] text-[.8vw] left-[3vw]">Página Inicial</h2>
                 </div>
             </Link>
         </div>
--- src/components/empresa.tsx
+++ src/components/empresa.tsx
@@ -3,10 +3,10 @@ import Image from "next/image"
 export function Empresa() {
     return(
         <div className="flex absolute w-full h-full top-0 bottom-0 left-0 right-0 m-auto items-center justify-center select-none">
-            <h1 className="absolute text-black md:text-[2.5vw] lg:text-[1.5vw] top-[5vw]">Sua empresa não foi localizada!</h1>
-            <h2 className="absolute text-black md:text-[1.2vw] lg:text-[.8vw] top-[8vw]">Informe sua administração ou a do site para corrigir!</h2>
+            <h1 className="absolute text-black text-[5vw] md:text-[2.5vw] lg:text-[1.5vw] top-[10vw] md:top-[5vw]">Sua empresa não foi localizada!</h1>
+            <h2 className="absolute text-black text-[1.8vw] md:text-[1.2vw] lg:text-[.8vw] top-[18vw] md:top-[8vw]">Informe sua administração ou a do site para corrigir!</h2>
             <Image
-                className="absolute md:w-[70vw] lg:w-[50vw]"
+                className="absolute w-[80vw] md:w-[70vw] lg:w-[50vw]"
                 src="/404.svg"
                 alt="Logo 404 erro"
                 width={180}
