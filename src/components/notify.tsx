5b5b08bd7e2f27175e2ef5e1db7b81a3588ffd2a
Peres <fabricioperesdsantos@gmail.com>
2025-08-30 17:58:34 -0300

    Ajuste do tab

--- src/components/notify.tsx
+++ src/components/notify.tsx
@@ -1,15 +1,15 @@
 'use client'
 
 export function Notify(msg: string) {
-    const div = document.createElement("div");
+  const div = document.createElement("div");
   
-    div.innerText = msg;
-    div.className = `fixed top-[1vw] left-1/2 transform -translate-x-1/2 bg-white text-[#111827] px-6 py-3 rounded shadow-lg border z-[9999] transition-opacity duration-300`;
+  div.innerText = msg;
+  div.className = `fixed top-[1vw] left-1/2 transform -translate-x-1/2 bg-white text-[#111827] px-6 py-3 rounded shadow-lg border z-[9999] transition-opacity duration-300`;
   
-    document.body.appendChild(div);
+  document.body.appendChild(div);
   
-    setTimeout(() => {
-      div.style.opacity = "0";
-      setTimeout(() => div.remove(), 300); 
-    }, 3000);
+  setTimeout(() => {
+    div.style.opacity = "0";
+    setTimeout(() => div.remove(), 300); 
+  }, 5000);
 }
