fdec45989539a17545762d6ea0201b65ae16c2e0
Peres <fabricioperesdsantos@gmail.com>
2025-08-30 17:58:13 -0300

    Ajuste do tab

--- src/components/pagina.tsx
+++ src/components/pagina.tsx
@@ -7,7 +7,8 @@ import { SideBar } from "./sidebar";
 const Pagina = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     var dark = useTheme().dark
 
-    return (<>
+    return (
+        <>
             <div className="md:flex absolute w-full md:w-[85.417vw] h-[90vh] top-[10vh] md:left-[14.583vw] items-center justify-center overflow-x-hidden overflow-y-auto" style={{ background: dark ? '#0B0A0A' : '#F9FAFC' }}>
                 <main>{children}</main>
             </div>
