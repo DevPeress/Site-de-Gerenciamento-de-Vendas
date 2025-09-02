b78aa602eabd5d52dcf014db2f37509d50e575d7
Peres <fabricioperesdsantos@gmail.com>
2025-09-01 22:05:22 -0300

    Ajuste do return

--- src/app/api/registerFuncionario/route.ts
+++ src/app/api/registerFuncionario/route.ts
@@ -5,9 +5,7 @@ export async function POST(req: Request) {
   const body = await req.json();
   const { idLoja, email } = body as { idLoja: number, email: string };
 
-  if (!idLoja || !email) {
-    return new NextResponse("Erro ao cadastrar", { status: 400 })
-  }
+  if (!idLoja || !email) return new NextResponse("Erro ao cadastrar", { status: 400 })
 
   try {
     const user = await prisma.funcionarios.create({
